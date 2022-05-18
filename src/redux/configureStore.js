2a05a595bbe6b0cfccbb4e9b3756c6d6b109a73c
Have Samuel <samiehave9@gmail.com>
2022-05-18 04:12:36 +0300

    Added the test folder

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import Reducer from '';
+import { applyMiddleware, combineReducers, configureStore } from 'redux';
+import thunk from 'redux-thunk';
+import AnimalReducer from './animals/animal';
 
-const reducer = Reducer;
-
-const store = configureStore({
-  reducer,
-  middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(logger),
-  devTools: process.env.NODE_ENV !== 'production',
+const root = combineReducers({
+  animal: AnimalReducer,
 });
+const store = configureStore(root, applyMiddleware(thunk));
 
 export default store;
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import logger from 'redux-logger';
+// import AnimalReducer from './animals/animal';
+
+// const reducer = Reducer;
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(logger),
+//   devTools: process.env.NODE_ENV !== 'production',
+// });
+
+// export default store;
